7b47e324bde9f3a7708bac5d58f0a9e6c3d16f36
Andres Polania <apolanialogyca.org>
2022-11-30 16:27:08 -0500

    Componente peticiones

--- src/app/peticiones-http/peticiones-http.component.ts
+++ src/app/peticiones-http/peticiones-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as dataInfo from '../dataInfo';
 
 @Component({
   selector: 'app-peticiones-http',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./peticiones-http.component.scss']
 })
 export class PeticionesHTTPComponent implements OnInit {
+
+  dataText = dataInfo;
+  cardHead = this.dataText.requests;
 
   constructor() { }
 
