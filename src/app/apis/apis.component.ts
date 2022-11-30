b88d7bf47a0da51dbe3dce216b6d69b3ee806211
Andres Polania <apolanialogyca.org>
2022-11-30 16:14:02 -0500

    Componente Apis

--- src/app/apis/apis.component.ts
+++ src/app/apis/apis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as dataInfo from '../dataInfo';
 
 @Component({
   selector: 'app-apis',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apis.component.scss']
 })
 export class ApisComponent implements OnInit {
+
+  dataText = dataInfo;
+  cardHead = this.dataText.apis;
 
   constructor() { }
 
